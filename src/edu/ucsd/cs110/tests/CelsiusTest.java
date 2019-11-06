0256e98371c10dcb4009543eb45820339f12b832
Yixing Wang <yxonestarw@gmail.com>
2019-11-06 10:53:02 -0800

    I don't why the push didn't show up

--- src/edu/ucsd/cs110/tests/CelsiusTest.java
+++ src/edu/ucsd/cs110/tests/CelsiusTest.java
@@ -34,8 +34,7 @@ public class CelsiusTest extends TestCase{
         // (Hint: what is the length of the middle of the string?)
         assertTrue(string.substring(0, endIndex).equals(beginning));
     }
-
-
+    
     public void testCelsiusToCelsius()
     {
         Celsius temp = new Celsius(0);
